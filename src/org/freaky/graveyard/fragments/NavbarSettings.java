24dd2e179d456be107f3b9f34e33940e0d58cae9
luk1337 <priv.luk@gmail.com>
2019-10-22 16:01:02 +0530

    FreakyGraveyard: Add 'Invert layout' to navigation bar settings
    Change-Id: Ic99baeee93c2463a476b7d48b8d6ed4786035b05
    Signed-off-by: bunnyyTheRuinous <hsinghalk@yahoo.com>

--- src/org/freaky/graveyard/fragments/NavbarSettings.java
+++ src/org/freaky/graveyard/fragments/NavbarSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2014 TeamEos
+ * Copyright (C) 2020 The FreakyOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.freaky.graveyard.fragments;
 
-import java.util.ArrayList;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.provider.Settings;
 import android.os.UserHandle;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.ListPreference;
@@ -30,25 +29,45 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
-import android.provider.Settings;
 
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.R;
 
-public class NavbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavbarSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    public static final String TAG = "Navigation";
+
+    private static final String NAV_BAR_LAYOUT = "nav_bar_layout";
+    private static final String SYSUI_NAV_BAR = "sysui_nav_bar";
+
+    private ListPreference mNavBarLayout;
+    private ContentResolver mResolver;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.freaky_graveyard_navigation);
+        mResolver = getActivity().getContentResolver();
 
+        mNavBarLayout = (ListPreference) findPreference(NAV_BAR_LAYOUT);
+        mNavBarLayout.setOnPreferenceChangeListener(this);
+        String navBarLayoutValue = Settings.Secure.getString(mResolver, SYSUI_NAV_BAR);
+        if (navBarLayoutValue != null) {
+            mNavBarLayout.setValue(navBarLayoutValue);
+        } else {
+            mNavBarLayout.setValueIndex(0);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
+        if (preference == mNavBarLayout) {
+            Settings.Secure.putString(mResolver, SYSUI_NAV_BAR, (String) newValue);
+            return true;
+        }
         return false;
     }
 
